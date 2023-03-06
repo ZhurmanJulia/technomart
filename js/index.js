a296e1c7065cf23b0ad534945c7e1d03ac9a08ac
ZhurmanJulia <zhurmanjulia@mail.ru>
2023-03-07 01:43:35 +1000

    Добавляет js для dropdown, проводит финальную проверку по PP

--- js/index.js
+++ js/index.js
@@ -28,3 +28,24 @@ buttonNext.addEventListener('click', (e) => {
   currentIndex = (currentIndex === slidesAmount) ? 0 : currentIndex;
   onSlideChange(currentIndex);
 });
+
+const modalOpenBtn = document.querySelector('.log-in-button');
+const modal = document.querySelector('.dropdown-form');
+const modalCloseBtn = document.querySelector('.dropdown-close');
+
+modalOpenBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+modal.classList.add('is-open');
+});
+
+modalCloseBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.remove('is-open');
+  });
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    modal.classList.remove('is-open');
+  }
+});
